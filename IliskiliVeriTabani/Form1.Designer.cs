7d945276305303a4eca74363b4f8e9b8712e7084
Ece <ecehiz.eh98@hotmail.com>
2023-05-16 15:44:37 +0300

    Add project files.

---
+++ IliskiliVeriTabani/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace IliskiliVeriTabani
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this._Db_IliskiliVeriTabaniDataSet = new IliskiliVeriTabani._Db_IliskiliVeriTabaniDataSet();
+            this.birlestirilmisTabloBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.birlestirilmisTabloTableAdapter = new IliskiliVeriTabani._Db_IliskiliVeriTabaniDataSetTableAdapters.birlestirilmisTabloTableAdapter();
+            this.ürünAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.müşteriDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fiyatDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._Db_IliskiliVeriTabaniDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.birlestirilmisTabloBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ürünAdDataGridViewTextBoxColumn,
+            this.müşteriDataGridViewTextBoxColumn,
+            this.personelDataGridViewTextBoxColumn,
+            this.fiyatDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.birlestirilmisTabloBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 562);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // _Db_IliskiliVeriTabaniDataSet
+            // 
+            this._Db_IliskiliVeriTabaniDataSet.DataSetName = "_Db_IliskiliVeriTabaniDataSet";
+            this._Db_IliskiliVeriTabaniDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // birlestirilmisTabloBindingSource
+            // 
+            this.birlestirilmisTabloBindingSource.DataMember = "birlestirilmisTablo";
+            this.birlestirilmisTabloBindingSource.DataSource = this._Db_IliskiliVeriTabaniDataSet;
+            // 
+            // birlestirilmisTabloTableAdapter
+            // 
+            this.birlestirilmisTabloTableAdapter.ClearBeforeFill = true;
+            // 
+            // ürünAdDataGridViewTextBoxColumn
+            // 
+            this.ürünAdDataGridViewTextBoxColumn.DataPropertyName = "ÜrünAd";
+            this.ürünAdDataGridViewTextBoxColumn.HeaderText = "ÜrünAd";
+            this.ürünAdDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ürünAdDataGridViewTextBoxColumn.Name = "ürünAdDataGridViewTextBoxColumn";
+            // 
+            // müşteriDataGridViewTextBoxColumn
+            // 
+            this.müşteriDataGridViewTextBoxColumn.DataPropertyName = "Müşteri";
+            this.müşteriDataGridViewTextBoxColumn.HeaderText = "Müşteri";
+            this.müşteriDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.müşteriDataGridViewTextBoxColumn.Name = "müşteriDataGridViewTextBoxColumn";
+            // 
+            // personelDataGridViewTextBoxColumn
+            // 
+            this.personelDataGridViewTextBoxColumn.DataPropertyName = "Personel";
+            this.personelDataGridViewTextBoxColumn.HeaderText = "Personel";
+            this.personelDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.personelDataGridViewTextBoxColumn.Name = "personelDataGridViewTextBoxColumn";
+            // 
+            // fiyatDataGridViewTextBoxColumn
+            // 
+            this.fiyatDataGridViewTextBoxColumn.DataPropertyName = "Fiyat";
+            this.fiyatDataGridViewTextBoxColumn.HeaderText = "Fiyat";
+            this.fiyatDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.fiyatDataGridViewTextBoxColumn.Name = "fiyatDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._Db_IliskiliVeriTabaniDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.birlestirilmisTabloBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private _Db_IliskiliVeriTabaniDataSet _Db_IliskiliVeriTabaniDataSet;
+        private System.Windows.Forms.BindingSource birlestirilmisTabloBindingSource;
+        private _Db_IliskiliVeriTabaniDataSetTableAdapters.birlestirilmisTabloTableAdapter birlestirilmisTabloTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ürünAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn müşteriDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fiyatDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ IliskiliVeriTabani/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IliskiliVeriTabani
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-RGH49P0Q;Initial Catalog=Db.IliskiliVeriTabani;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("exec birlestirilmisTablo", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
